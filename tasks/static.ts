7d77e5fc306a655542c7c2ba60ed155ced74595f
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2022-06-11 08:49:46 +1000

    Fix static task erroring if something is modified twice in a row

--- tasks/static.ts
+++ tasks/static.ts
@@ -1,4 +1,7 @@
 import fs from "fs-extra";
 import Task from "./utilities/Task";
 
-export default Task("static", () => fs.copy("static", "docs"));
+export default Task("static", async () => {
+	while (!await fs.copy("static", "docs")
+		.then(() => true).catch(() => false));
+});
