606758dc5f337498571bb5d49f9b2e1c03866b69
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2022-07-07 22:47:21 +1000

    Mark stat distribution and stat total as inapplicable on class items

--- src/ui/view/inventory/InventoryClassItemView.ts
+++ src/ui/view/inventory/InventoryClassItemView.ts
@@ -1,7 +1,15 @@
+import Sort from "ui/inventory/sort/Sort";
+import SortManager from "ui/inventory/sort/SortManager";
 import InventoryArmourView from "ui/view/inventory/InventoryArmourView";
 
 export default InventoryArmourView.create({
 	id: "class-item",
 	name: "Class Item",
 	slot: slots => slots.byName("ClassItems"),
+	sort: new SortManager({
+		id: "class-items",
+		name: "Class Items",
+		default: [Sort.Power, Sort.Name, Sort.Energy],
+		inapplicable: [Sort.Deepsight, Sort.Pattern, Sort.StatDistribution, Sort.StatTotal],
+	}),
 });
