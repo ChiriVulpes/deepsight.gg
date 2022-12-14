f63ed207c42170e36042c0456679351162594f91
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2022-12-14 17:29:13 +1300

    Fix details element contents not being inert when closed

--- src/ui/Details.ts
+++ src/ui/Details.ts
@@ -28,23 +28,29 @@ export default class Details<ARGS extends readonly any[] = []> extends Component
 
 	public open () {
 		this.classes.remove(DetailsClasses.Closed);
+		for (const child of this.element.children)
+			child.removeAttribute("inert");
+
 		this.event.emit("toggle");
 		return this;
 	}
 
 	public close () {
 		this.classes.add(DetailsClasses.Closed);
+		for (const child of this.element.children)
+			if (child !== this.summary.element)
+				child.setAttribute("inert", "");
 		this.event.emit("toggle");
 		return this;
 	}
 
 	public toggle (open?: boolean) {
-		if (open === undefined || !this.classes.has(DetailsClasses.Closed) !== open) {
-			this.classes.toggle(DetailsClasses.Closed);
-			this.event.emit("toggle");
-		}
+		const isOpen = !this.classes.has(DetailsClasses.Closed);
+		if (open !== undefined && isOpen === open)
+			return this;
 
-		return this;
+		open = open === undefined ? !isOpen : open;
+		return open ? this.open() : this.close();
 	}
 
 	public override removeContents (): this {
