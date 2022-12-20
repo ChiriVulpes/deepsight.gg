65f5f18ffc5ac7e1b721e45e229a743b672040d2
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2022-12-20 19:19:31 +1300

    Collections: Split items into weapons, and armour by sorted classes

--- src/ui/view/collections/CollectionsView.ts
+++ src/ui/view/collections/CollectionsView.ts
@@ -15,7 +15,9 @@ import View from "ui/View";
 
 export enum CollectionsViewClasses {
 	Bucket = "view-collections-bucket",
-	BucketDetails = "view-collections-bucket-details",
+	BucketTitle = "view-collections-bucket-title",
+	Source = "view-collections-source",
+	SourceContent = "view-collections-source-content",
 }
 
 export default View.create({
@@ -45,7 +47,7 @@ export default View.create({
 				defaultOpen = true;
 
 			const details = Details.create()
-				.classes.add(CollectionsViewClasses.BucketDetails)
+				.classes.add(CollectionsViewClasses.Source)
 				.toggle(defaultOpen)
 				.tweak(details => details.summary.text.set(source.displayProperties.name))
 				.appendTo(view.content);
@@ -58,20 +60,65 @@ export default View.create({
 			}))
 				.onReady(items => {
 					console.log(source.displayProperties.name, items);
-					return Component.create()
+					const weapons: Item[] = [];
+					const classItems: Partial<Record<DestinyClass, Item[]>> = {};
+
+					for (const item of items) {
+						if (item.definition.itemCategoryHashes?.includes(ItemCategoryHashes.Weapon)) {
+							weapons.push(item);
+							continue;
+						}
+
+						(classItems[item.definition.classType] ??= [])
+							.push(item);
+					}
+
+					const wrapper = Component.create()
+						.classes.add(CollectionsViewClasses.SourceContent);
+
+					Component.create()
 						.classes.add(CollectionsViewClasses.Bucket)
-						.append(...items
-							.sort((a, b) => getSortIndex(b) - getSortIndex(a)
-								|| (Display.name(a.definition) ?? "").localeCompare(Display.name(b.definition) ?? ""))
-							.map(item => Component.create()
-								.classes.add(InventoryClasses.Slot)
-								.append(ItemComponent.create([item, inventory]))));
+						.append(Component.create()
+							.classes.add(CollectionsViewClasses.BucketTitle)
+							.text.set("Weapons"))
+						.tweak(addItems, weapons, inventory)
+						.appendTo(wrapper);
+
+					const classes = [DestinyClass.Titan, DestinyClass.Hunter, DestinyClass.Warlock]
+						.sort((a, b) => (inventory.sortedCharacters?.findIndex(character => character.classType === a) ?? 0)
+							- (inventory.sortedCharacters?.findIndex(character => character.classType === b) ?? 0));
+
+					for (const cls of classes) {
+						const items = classItems[cls];
+						if (!items?.length)
+							continue;
+
+						Component.create()
+							.classes.add(CollectionsViewClasses.Bucket)
+							.append(Component.create()
+								.classes.add(CollectionsViewClasses.BucketTitle)
+								.text.set(cls === DestinyClass.Titan ? "Titan" : cls === DestinyClass.Hunter ? "Hunter" : "Warlock")
+								.text.add(" Armor"))
+							.tweak(addItems, items, inventory)
+							.appendTo(wrapper);
+					}
+
+					return wrapper;
 				})
 				.setSimple()
 				.appendTo(details);
 		}
 	},
 });
+
+function addItems (component: Component, items: Item[], inventory: Inventory) {
+	component.append(...items
+		.sort((a, b) => getSortIndex(b) - getSortIndex(a)
+			|| (Display.name(a.definition) ?? "").localeCompare(Display.name(b.definition) ?? ""))
+		.map(item => Component.create()
+			.classes.add(InventoryClasses.Slot)
+			.append(ItemComponent.create([item, inventory]))));
+}
 
 function getSortIndex (item: Item) {
 	return (item.definition.itemCategoryHashes?.includes(ItemCategoryHashes.Weapon) ? 10000 : 0)
