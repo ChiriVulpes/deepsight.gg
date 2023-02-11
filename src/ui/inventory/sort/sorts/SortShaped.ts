97587c766b0d98ebe4bb873d05408409ecb12c16
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2023-02-11 14:04:04 +1300

    Make shaped sort sort weapons by levelling progress too

--- src/ui/inventory/sort/sorts/SortShaped.ts
+++ src/ui/inventory/sort/sorts/SortShaped.ts
@@ -1,7 +1,14 @@
+import type Item from "model/models/items/Item";
 import Sort, { ISort } from "ui/inventory/sort/Sort";
 
 export default ISort.create({
 	id: Sort.Shaped,
 	name: "Shaped",
-	sort: (a, b) => Number(!!b.shaped) - Number(!!a.shaped),
+	sort: (a, b) => getSortIndex(b) - getSortIndex(a),
 });
+
+function getSortIndex (item: Item) {
+	return Number(!!item.shaped) * 10000000
+		+ (item.shaped?.level?.progress.progress ?? 0) * 10000
+		+ (item.shaped?.progress?.progress.progress ?? 0);
+}
