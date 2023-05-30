6fe8351396afa905e4de063ed32fbdd7685d3ad9
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2023-05-31 10:42:07 +1200

    Fix Destiny 2 source

--- src/model/models/Manifest.ts
+++ src/model/models/Manifest.ts
@@ -60,7 +60,7 @@ const Manifest = Model.create("manifest", {
 	cache: "Global",
 	version: async () => {
 		const manifest = await GetManifest.query();
-		return `${manifest.version}-11.deepsight.gg`;
+		return `${manifest.version}-12.deepsight.gg`;
 	},
 	async generate (api) {
 		await Model.cacheDB.dispose();
