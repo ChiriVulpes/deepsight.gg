1d9640a7574908f80412e1614bf27193aedfd63a
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2023-10-07 23:34:45 +1300

    Improve appearance of some basic clarity tooltip stuff by parsing the text

--- src/ui/inventory/tooltip/ItemClarity.ts
+++ src/ui/inventory/tooltip/ItemClarity.ts
@@ -2,6 +2,7 @@ import { ClarityManifest } from "model/models/Manifest";
 import type { Plug } from "model/models/items/Plugs";
 import { Classes } from "ui/Classes";
 import Component from "ui/Component";
+import Env from "utility/Env";
 import type { ClarityDescriptionComponent } from "utility/endpoint/clarity/endpoint/GetClarityDescriptions";
 
 export enum ItemClarityClasses {
@@ -11,6 +12,11 @@ export enum ItemClarityClasses {
 	Logo = "item-plug-tooltip-clarity-logo",
 	Description = "item-plug-tooltip-clarity-description",
 	Line = "item-plug-tooltip-clarity-line",
+	Label = "item-plug-tooltip-clarity-label",
+	Numeric = "item-plug-tooltip-clarity-numeric",
+	StackSizeSeparator = "item-plug-tooltip-clarity-stack-size-separator",
+	EnhancedArrow = "item-plug-tooltip-clarity-enhanced-arrow",
+	EnhancedLine = "item-plug-tooltip-clarity-enhanced-line",
 }
 
 export default class ItemClarity extends Component {
@@ -50,23 +56,72 @@ export default class ItemClarity extends Component {
 			return false;
 
 		this.classes.remove(Classes.Hidden);
-		addClarityDescriptionComponents(this.description, clarityDescription.descriptions.en);
+		appendClarityDescriptionComponents(this.description, clarityDescription.descriptions.en);
 
 		return true;
 	}
 }
 
-function addClarityDescriptionComponents (parent: Component, content: string | ClarityDescriptionComponent[]) {
+function appendClarityDescriptionComponents (parent: Component, content: string | ClarityDescriptionComponent[]) {
 	if (typeof content === "string")
-		return Component.create("span")
-			.text.set(content)
-			.appendTo(parent);
+		content = [{ text: content }];
 
 	for (const component of content) {
-		Component.create(component.linesContent?.length ? "div" : component.classNames?.includes("spacer") ? "br" : "span")
-			.classes.add(...component.linesContent?.length ? [ItemClarityClasses.Line] : [], ...component.classNames ?? [])
-			.text.set(component.text ?? "")
-			.tweak(addClarityDescriptionComponents, component.linesContent ?? [])
+		const isLine = !!component.linesContent?.length;
+		const isLabel = isLine && extractText(component.linesContent![component.linesContent!.length - 1]).trimEnd().endsWith(":");
+		const isEnhancedEffect = isLine && extractText(component.linesContent![0]).trimStart().startsWith("🡅");
+		const isEnhancedArrow = component.classNames?.includes("enhancedArrow") ?? false;
+		const isSpacer = component.classNames?.includes("spacer") ?? false;
+		Component.create(isLine ? "div" : isSpacer ? "br" : "span")
+			.classes.toggle(isLine, ItemClarityClasses.Line)
+			.classes.toggle(isEnhancedEffect, ItemClarityClasses.EnhancedLine)
+			.classes.toggle(isLabel, ItemClarityClasses.Label)
+			.classes.toggle(isEnhancedArrow, ItemClarityClasses.EnhancedArrow)
+			.classes.add(...isLine ? [ItemClarityClasses.Line] : [], ...Env.DEEPSIGHT_ENVIRONMENT === "dev" ? component.classNames ?? [] : [])
+			.tweak(appendClarityText, isEnhancedArrow ? "" : component.text ?? "", component.classNames ?? [])
+			.tweak(appendClarityDescriptionComponents, component.linesContent ?? [])
 			.appendTo(parent);
 	}
+}
+
+const numericSplitRegex = /(?<![\d.])(?=\d)|(?<=[\d%x+])(?![\d%x+.])/g;
+const stackSizeSplitRegex = /(?=\|)|(?<=\|)/g;
+function appendClarityText (parent: Component, text: string, classNames: string[]) {
+	const sections = text.split(numericSplitRegex);
+	for (const section of sections) {
+		if (!section)
+			continue;
+
+		if (isNaN(parseFloat(section))) {
+			const sections = section.split(stackSizeSplitRegex);
+			for (const section of sections) {
+				if (section.trim() === "|")
+					Component.create("span")
+						.classes.add(ItemClarityClasses.StackSizeSeparator)
+						.text.set("/")
+						.appendTo(parent);
+				else
+					parent.text.add(section);
+			}
+			continue;
+		}
+
+		Component.create("span")
+			.classes.add(ItemClarityClasses.Numeric)
+			.text.set(section)
+			.appendTo(parent);
+	}
+}
+
+function extractText (content: ClarityDescriptionComponent | string | ClarityDescriptionComponent[]) {
+	content = typeof content === "string" ? [{ text: content }] : Array.isArray(content) ? content : [content];
+
+	let result = "";
+	for (const component of content) {
+		result += component.text ?? "";
+		if (component.linesContent?.length)
+			result += extractText(component.linesContent);
+	}
+
+	return result;
 }
