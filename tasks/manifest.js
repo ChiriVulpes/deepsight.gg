34758b23bcecbd05ba336216986b5c954bb4795e
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2023-11-03 21:29:17 +1300

    Implement check-manifest internal js file

---
+++ tasks/manifest.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment */
+import fs from "fs/promises";
+
+void (async () => {
+	let savedVersion;
+	try {
+		const versionsString = await fs.readFile("manifest/versions.json", "utf8");
+		const versions = JSON.parse(versionsString) ?? {};
+		savedVersion = versions["Destiny2/Manifest"];
+	} catch {
+		return;
+	}
+
+	let manifest;
+	for (let attempts = 0; !manifest && attempts < 10; attempts++) {
+		manifest = await fetch("https://www.bungie.net/Platform/Destiny2/Manifest/")
+			.then(response => response.json())
+			.then(json => {
+				const manifest = json.Response;
+				if (!manifest)
+					console.warn(`Bungie API did not return a valid manifest: ${JSON.stringify(json)}`);
+				return json.Response;
+			});
+
+		if (!manifest)
+			await new Promise(resolve => setTimeout(resolve, 1000));
+	}
+
+	if (!manifest)
+		throw new Error("Unable to get current manifest version");
+
+	const bungieVersion = manifest.version;
+	if (savedVersion !== bungieVersion)
+		return;
+
+	throw new Error("No manifest update");
+})();
