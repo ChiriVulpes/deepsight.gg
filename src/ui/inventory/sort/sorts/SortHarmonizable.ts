d89d448cf9a64fb947c99f46dd39441de518b30c
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2023-11-26 17:05:31 +1300

    Display pattern progress for harmonizable sort

--- src/ui/inventory/sort/sorts/SortHarmonizable.ts
+++ src/ui/inventory/sort/sorts/SortHarmonizable.ts
@@ -1,4 +1,5 @@
 import type Item from "model/models/items/Item";
+import Component from "ui/Component";
 import Sort, { ISort } from "ui/inventory/sort/Sort";
 
 export default ISort.create({
@@ -6,9 +7,27 @@ export default ISort.create({
 	name: "Deepsight Activation",
 	shortName: "Activation",
 	renderSortable: sortable => sortable.icon,
+	render: item => {
+		if (!item.deepsight?.activation)
+			return undefined;
+
+		return Component.create()
+			.classes.add("item-sort-harmonizable")
+			.append(Component.create("span")
+				.classes.add("item-sort-harmonizable-icon"))
+			.append(Component.create("span")
+				.classes.add("item-sort-harmonizable-numerator")
+				.text.set(`${item.deepsight?.pattern?.progress?.progress ?? 0}`))
+			.append(Component.create("span")
+				.classes.add("item-sort-harmonizable-separator")
+				.text.set("/"))
+			.append(Component.create("span")
+				.classes.add("item-sort-harmonizable-denominator")
+				.text.set(`${item.deepsight?.pattern?.progress?.completionValue}`));
+	},
 	sort: (a, b) => getSortIndex(b) - getSortIndex(a),
 });
 
 function getSortIndex (item: Item) {
-	return Number(!!item.deepsight?.activation);
+	return Number(!!item.deepsight?.activation && (item.deepsight?.pattern?.progress?.progress ?? 0) + 1);
 }
