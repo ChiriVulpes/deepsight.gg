9207c6f078a9286e42d12335528549e132444220
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2023-11-29 11:27:48 +1300

    *actually* fix reset times for real this time

--- src/utility/endpoint/bungie/Bungie.ts
+++ src/utility/endpoint/bungie/Bungie.ts
@@ -22,12 +22,12 @@ export class BungieAPI {
 	}
 
 	public get lastWeeklyReset () {
-		const day = (new Date().getUTCDay() - 2) % 7;
+		const day = (7 - (new Date().getUTCDay() + 5)) % 7;
 		return this.lastDailyReset - day * Time.days(1);
 	}
 
 	public get lastTrialsReset () {
-		const day = (new Date().getUTCDay() + 2) % 7;
+		const day = (7 - (new Date().getUTCDay() + 1)) % 7;
 		return this.lastDailyReset - day * Time.days(1);
 	}
 
