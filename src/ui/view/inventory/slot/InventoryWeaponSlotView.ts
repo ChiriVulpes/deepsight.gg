2960ae92e3c3f2ae6c3bb98247a4cfeb254742de
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2023-12-13 02:29:00 +1300

    Use a better default sort

--- src/ui/view/inventory/slot/InventoryWeaponSlotView.ts
+++ src/ui/view/inventory/slot/InventoryWeaponSlotView.ts
@@ -5,7 +5,7 @@ import type { ISortManagerConfiguration } from "ui/inventory/sort/SortManager";
 import SortManager from "ui/inventory/sort/SortManager";
 import InventorySlotView from "ui/view/inventory/slot/InventorySlotView";
 
-export const SORTS_DEFAULT_WEAPONS = [Sort.Pattern, Sort.Power, Sort.Name] as const;
+export const SORTS_DEFAULT_WEAPONS = [Sort.Pattern, Sort.Rarity, Sort.Masterwork, Sort.Power, Sort.DamageType, Sort.AmmoType] as const;
 export const SORTS_INAPPLICABLE_WEAPONS = [Sort.Energy, Sort.StatTotal, Sort.StatDistribution, Sort.Quantity, "stat-.*"] as const;
 export const FILTERS_INAPPLICABLE_WEAPONS = [] as const;
 
