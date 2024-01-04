dceb793f46f9e4d06536e1b4361155ac20def440
Lumina Mystere <minimotogo@gmail.com>
2024-01-04 19:22:51 +1300

    Port deep stone crypt to new system

---
+++ tasks/manifest/droptable/DeepStoneCrypt.ts
@@ -0,0 +1,150 @@
+import { ActivityHashes, InventoryItemHashes, RecordHashes } from "../Enums";
+import { DeepsightDropTableDefinition } from "./DeepsightDropTableDefinition";
+
+
+export default {
+    hash: ActivityHashes.DeepStoneCrypt_RewardsLength0,
+    displayProperties: {
+        icon: { DestinyRecordDefinition: RecordHashes.DeepStoneCrypt3185876102 }
+    },
+    encounters: [
+        {
+            traversal: true,
+            displayProperties: {
+                name: "Locate the Deep Stone Crypt",
+                description: "Survive the cold while locating the Deep Stone Crypt",
+            },
+        },
+        {
+            phaseHash: 2776463390,
+            displayProperties: {
+                name: "Crypt Security",
+                directive: "Disable Crypt Security",
+                description: "Destroy the Crypt's power system to disable security.",
+            },
+            dropTable: {
+                [InventoryItemHashes.TrusteeScoutRifle_ItemType3]: {},
+
+                // hunter
+                [InventoryItemHashes.LegacysOathGripsGauntletsPlug]: {},
+                [InventoryItemHashes.LegacysOathStridesLegArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathCloakHunterCloakPlug]: {},
+
+                // warlock
+                [InventoryItemHashes.LegacysOathGlovesGauntletsPlug]: {},
+                [InventoryItemHashes.LegacysOathBootsLegArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathBondWarlockBondPlug]: {},
+
+                // titan
+                [InventoryItemHashes.LegacysOathGauntletsGauntletsPlug]: {},
+                [InventoryItemHashes.LegacysOathGreavesLegArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathMarkTitanMarkPlug]: {},
+            },
+        },
+        {
+            traversal: true,
+            phaseHash: 3847348336,
+            displayProperties: {
+                name: "Locate Eramis's Followers",
+                description: "Push deeper into the Crypt and locate Eramis's followers.",
+            },
+        },
+        {
+            phaseHash: 416127450,
+            displayProperties: {
+                name: "Atraks-1, Fallen Exo",
+                directive: "Defeat Atraks-1",
+                description: "Take down the Fallen Exo Atraks-1."
+            },
+            dropTable: {
+                [InventoryItemHashes.SuccessionSniperRifle_ItemType3]: {},
+                [InventoryItemHashes.HeritageShotgun_ItemType3]: {},
+
+                // hunter
+                [InventoryItemHashes.LegacysOathGripsGauntletsPlug]: {},
+                [InventoryItemHashes.LegacysOathStridesLegArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathCloakHunterCloakPlug]: {},
+
+                // warlock
+                [InventoryItemHashes.LegacysOathGlovesGauntletsPlug]: {},
+                [InventoryItemHashes.LegacysOathBootsLegArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathBondWarlockBondPlug]: {},
+
+                // titan
+                [InventoryItemHashes.LegacysOathGauntletsGauntletsPlug]: {},
+                [InventoryItemHashes.LegacysOathGreavesLegArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathMarkTitanMarkPlug]: {},
+            },
+        },
+        {
+            traversal: true,
+            phaseHash: 1370965191,
+            displayProperties: {
+                name: "Locate the Nuclear Contingency Chamber",
+                description: "Reach the nuclear contingency chamber."
+            },
+        },
+        {
+            phaseHash: 1858926029,
+            displayProperties: {
+                name: "Descent",
+                directive: "Prevent Europa's Destruction",
+                description: "Disarm the Nuclear Descent Protocol",
+            },
+            dropTable: {
+                [InventoryItemHashes.PosterityHandCannon_ItemType3]: {},
+
+                // hunter
+                [InventoryItemHashes.LegacysOathGripsGauntletsPlug]: {},
+                [InventoryItemHashes.LegacysOathVestChestArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathCloakHunterCloakPlug]: {},
+
+                // warlock
+                [InventoryItemHashes.LegacysOathGlovesGauntletsPlug]: {},
+                [InventoryItemHashes.LegacysOathRobesChestArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathBondWarlockBondPlug]: {},
+
+                // titan
+                [InventoryItemHashes.LegacysOathGauntletsGauntletsPlug]: {},
+                [InventoryItemHashes.LegacysOathPlateChestArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathMarkTitanMarkPlug]: {},
+            },
+        },
+        {
+            traversal: true,
+            phaseHash: 1594577984,
+            displayProperties: {
+                name: "Emerge from the Wreckage",
+                description: "Make your way out of the wreckage of the Morning Star.",
+            },
+        },
+        {
+            phaseHash: 4035296150,
+            displayProperties: {
+                name: "Taniks, the Abomination",
+                directive: "Defeat Taniks, the Abomination",
+                description: "Defeat Taniks for good."
+            },
+            dropTable: {
+                [InventoryItemHashes.BequestSword_ItemType3]: {},
+                [InventoryItemHashes.CommemorationMachineGun_ItemType3]: {},
+                [InventoryItemHashes.EyesOfTomorrowRocketLauncher]: {},
+
+                // hunter
+                [InventoryItemHashes.LegacysOathMaskHelmetPlug]: {},
+                [InventoryItemHashes.LegacysOathVestChestArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathStridesLegArmorPlug]: {},
+
+                // warlock
+                [InventoryItemHashes.LegacysOathCowlHelmetPlug]: {},
+                [InventoryItemHashes.LegacysOathRobesChestArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathBootsLegArmorPlug]: {},
+
+                // titan
+                [InventoryItemHashes.LegacysOathHelmHelmetPlug]: {},
+                [InventoryItemHashes.LegacysOathPlateChestArmorPlug]: {},
+                [InventoryItemHashes.LegacysOathGreavesLegArmorPlug]: {},
+            },
+        },
+    ],
+} satisfies DeepsightDropTableDefinition;
