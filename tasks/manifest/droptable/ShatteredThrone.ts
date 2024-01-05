8a7067a7e5210f13710a3fd2219f45dd475d45b6
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2024-01-06 06:14:10 +1300

    Port shattered throne into new system

---
+++ tasks/manifest/droptable/ShatteredThrone.ts
@@ -0,0 +1,110 @@
+import { ActivityHashes, InventoryItemHashes, RecordHashes } from "../Enums";
+import type { DeepsightDropTableDefinition } from "./DeepsightDropTableDefinition";
+
+export default {
+	hash: ActivityHashes.TheShatteredThrone,
+	displayProperties: {
+		icon: { DestinyRecordDefinition: RecordHashes.DarkMonastery },
+	},
+	dropTable: {
+		[InventoryItemHashes.AbideTheReturnSword_QualityCurrentVersion1]: {},
+		[InventoryItemHashes.RetoldTaleShotgun_QualityCurrentVersion1]: {},
+		[InventoryItemHashes.SleeplessRocketLauncher_QualityCurrentVersion1]: {},
+		[InventoryItemHashes.TigerspiteAutoRifle_QualityCurrentVersion1]: {},
+		[InventoryItemHashes.TwilightOathSniperRifle_QualityCurrentVersion1]: {},
+		[InventoryItemHashes.VouchsafeScoutRifle_QualityCurrentVersion1]: {},
+		[InventoryItemHashes.WakingVigilHandCannon_QualityCurrentVersion1]: {},
+
+		[InventoryItemHashes.ReverieDawnHelmHelmetPlug1472713738]: {},
+		[InventoryItemHashes.ReverieDawnGauntletsGauntletsPlug1478378067]: {},
+		[InventoryItemHashes.ReverieDawnPlateChestArmorPlug2561756285]: {},
+		[InventoryItemHashes.ReverieDawnGreavesLegArmorPlug788771493]: {},
+		[InventoryItemHashes.ReverieDawnMarkTitanMarkPlug4023744176]: {},
+
+		[InventoryItemHashes.ReverieDawnCasqueHelmetPlug2804026582]: {},
+		[InventoryItemHashes.ReverieDawnGraspsGauntletsPlug4008120231]: {},
+		[InventoryItemHashes.ReverieDawnHauberkChestArmorPlug3368092113]: {},
+		[InventoryItemHashes.ReverieDawnStridesLegArmorPlug3185383401]: {},
+		[InventoryItemHashes.ReverieDawnCloakHunterCloakPlug844097260]: {},
+
+		[InventoryItemHashes.ReverieDawnHoodHelmetPlug1076538039]: {},
+		[InventoryItemHashes.ReverieDawnGlovesGauntletsPlug150052158]: {},
+		[InventoryItemHashes.ReverieDawnTabardChestArmorPlug757360370]: {},
+		[InventoryItemHashes.ReverieDawnBootsLegArmorPlug569434520]: {},
+		[InventoryItemHashes.ReverieDawnBondWarlockBondPlug1394177923]: {},
+	},
+	encounters: [
+		{
+			traversal: true,
+			displayProperties: {
+				name: "Cross the Ascendant Plane",
+				description: "\"A throne world, shattered…\"",
+			},
+		},
+		{
+			displayProperties: {
+				name: "Search for safe passage",
+				description: "\"Show me your cunning…\"",
+			},
+		},
+		{
+			traversal: true,
+			displayProperties: {
+				name: "Journey to the spire",
+				description: "\"There is no place better suited for you…\"",
+			},
+		},
+		{
+			traversal: true,
+			displayProperties: {
+				name: "Press onward",
+				description: "\"Her plan will never come to pass…\"",
+			},
+		},
+		{
+			traversal: true,
+			displayProperties: {
+				name: "Cross the chasm",
+				description: "\"Look down. Listen to the Deep call to you…\"",
+			},
+		},
+		{
+			displayProperties: {
+				name: "Vorgeth, the Boundless Hunger",
+				directive: "Overcome the Keeper of Petitions",
+				description: "\"Shape your schemes into reality…\"",
+			},
+			dropTable: {
+				[InventoryItemHashes.WishEnderCombatBow]: {
+					requiresQuest: InventoryItemHashes.HuntersRemembranceQuestStep_Step2,
+					requiresItems: [
+						InventoryItemHashes.WakingTokenOfQuerimDummy_ItemType0,
+						InventoryItemHashes.WakingTokenOfEriviksDummy_ItemType0,
+						InventoryItemHashes.WakingTokenOfXavothDummy_ItemType0,
+					],
+				},
+			},
+		},
+		{
+			traversal: true,
+			displayProperties: {
+				name: "Press onward",
+				description: "\"Come closer…\"",
+			},
+		},
+		{
+			traversal: true,
+			displayProperties: {
+				name: "Ascend",
+				description: "\"Doomed are the hesitant…\"",
+			},
+		},
+		{
+			displayProperties: {
+				name: "Dûl Incaru, the Eternal Return",
+				directive: "Face Dûl Incaru",
+				description: "\"Enter the infinite…\"",
+			},
+		},
+	],
+} satisfies DeepsightDropTableDefinition;
