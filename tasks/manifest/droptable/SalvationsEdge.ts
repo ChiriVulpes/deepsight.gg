4dd3f0bf79355dae1c97e9c14129c809459feb1c
Lumina Mystere <minimotogo@gmail.com>
2024-06-19 18:36:06 +1200

    Add directives + descriptions to Salvation's Edge drop table

--- tasks/manifest/droptable/SalvationsEdge.ts
+++ tasks/manifest/droptable/SalvationsEdge.ts
@@ -15,6 +15,7 @@ export default {
 			},
 		},
 		{
+			phaseHash: 2761002327,
 			displayProperties: {
 				name: "Substratum",
 				directive: "Gain Access to the Monolith",
@@ -40,8 +41,18 @@ export default {
 			},
 		},
 		{
+			traversal: true,
+			displayProperties: {
+				name: "Enter the Monolith's Core",
+				description: "Find a way to ascend the monolith.",
+			},
+		},
+		{
+			phaseHash: 193978048,
 			displayProperties: {
 				name: "Dissipation",
+				directive: "Defeat the Herald",
+				description: "Remove the Herald of Finality from your path.",
 			},
 			dropTable: {
 				[InventoryItemHashes.SummumBonumSword_ItemType3]: {},
@@ -64,8 +75,18 @@ export default {
 			},
 		},
 		{
+			traversal: true,
+			displayProperties: {
+				name: "Ascend the Monolith",
+				description: "Progress up the monolith and stop the Witness.",
+			},
+		},
+		{
+			phaseHash: 1727550020,
 			displayProperties: {
 				name: "Repository",
+				directive: "Carve A Path",
+				description: "Gain access to the Witness.",
 			},
 			dropTable: {
 				[InventoryItemHashes.NullifyPulseRifle_ItemType3]: {},
@@ -84,8 +105,18 @@ export default {
 			},
 		},
 		{
+			traversal: true,
+			displayProperties: {
+				name: "Ascend the Monolith",
+				description: "Continue to ascend the monolith.",
+			},
+		},
+		{
+			phaseHash: 637313410,
 			displayProperties: {
 				name: "Verity",
+				directive: "See Beyond",
+				description: "Find a path to ascension.",
 			},
 			dropTable: {
 				[InventoryItemHashes.SummumBonumSword_ItemType3]: {},
@@ -104,8 +135,18 @@ export default {
 			},
 		},
 		{
+			traversal: true,
+			displayProperties: {
+				name: "Make Your Final Ascent",
+				description: "Climb to the summit and confront the Witness.",
+			},
+		},
+		{
+			phaseHash: 4077323831,
 			displayProperties: {
 				name: "Zenith",
+				directive: "Stop the Final Shape",
+				description: "Free the Traveler's Light and thwart the Witness's plans.",
 			},
 			dropTable: {
 				[InventoryItemHashes.EuphonyLinearFusionRifle]: {},
