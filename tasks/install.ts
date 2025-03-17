ebb0cdb50be6cf79fa0494e3ebe6f93458c3bf45
Chiri Vulpes <chiri@chiri.works>
2025-03-17 15:50:27 +1300

    Only update bungie-api-ts in dev, always install latest deepsight.gg

--- tasks/install.ts
+++ tasks/install.ts
@@ -4,9 +4,13 @@ import Log from "./utility/Log";
 import Task from "./utility/Task";
 
 export default Task("install", async () => {
+	await Task.cli({ cwd: "src" }, "PATH:npm", "install");
+
 	if (Env.DEEPSIGHT_ENVIRONMENT === "dev") {
-		await Task.cli({ cwd: "src" }, "PATH:npm", "install");
-		Log.info(`Installing ${ansi.lightCyan("bungie-api-ts@latest")} and ${ansi.lightCyan("deepsight.gg@latest")}...`);
-		await Task.cli({ cwd: "src" }, "PATH:npm", "install", "bungie-api-ts@latest", "deepsight.gg@latest");
+		Log.info(`Installing ${ansi.lightCyan("bungie-api-ts@latest")}...`);
+		await Task.cli({ cwd: "src" }, "PATH:npm", "install", "bungie-api-ts@latest", "--no-fund", "--prefer-online", "--no-audit");
 	}
+
+	Log.info(`Installing ${ansi.lightCyan("deepsight.gg@latest")}...`);
+	await Task.cli({ cwd: "src" }, "PATH:npm", "install", "deepsight.gg@latest", "--no-fund", "--prefer-online", "--no-audit");
 });
