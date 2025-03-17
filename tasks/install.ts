45670ee8bbeabc98f99e7891cfdc8497df293026
Chiri Vulpes <chiri@chiri.works>
2025-03-17 15:45:46 +1300

    Fix undoing updating deepsight.gg version in build

--- tasks/install.ts
+++ tasks/install.ts
@@ -4,8 +4,8 @@ import Log from "./utility/Log";
 import Task from "./utility/Task";
 
 export default Task("install", async () => {
-	await Task.cli({ cwd: "src" }, "PATH:npm", "install");
 	if (Env.DEEPSIGHT_ENVIRONMENT === "dev") {
+		await Task.cli({ cwd: "src" }, "PATH:npm", "install");
 		Log.info(`Installing ${ansi.lightCyan("bungie-api-ts@latest")} and ${ansi.lightCyan("deepsight.gg@latest")}...`);
 		await Task.cli({ cwd: "src" }, "PATH:npm", "install", "bungie-api-ts@latest", "deepsight.gg@latest");
 	}
