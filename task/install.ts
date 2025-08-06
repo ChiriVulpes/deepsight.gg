7be771ed37c1bf304d98c3a922d1d90c6fdee94c
Chiri Vulpes <chiri@chiri.works>
2025-08-07 09:51:48 +1200

    Display sources via DeepsightItemSourceDefinition

--- task/install.ts
+++ task/install.ts
@@ -6,17 +6,17 @@ export default Task('install', async task => {
 		{
 			path: '.',
 			dependencies: {
-				task: { path: 'chirivulpes/task', branch: 'package' },
-				lint: { path: 'fluff4me/lint' },
-				chiri: { path: 'fluff4me/chiri', branch: 'package' },
-				weaving: { path: 'chirivulpes/weaving', branch: 'package' },
+				task: { repo: 'chirivulpes/task', branch: 'package' },
+				lint: { repo: 'fluff4me/lint' },
+				chiri: { repo: 'fluff4me/chiri', branch: 'package' },
+				weaving: { repo: 'chirivulpes/weaving', branch: 'package' },
 			},
 		},
 		{
 			path: 'src',
 			dependencies: {
-				'kitsui': { path: 'fluff4me/kitsui', branch: 'package' },
-				'conduit.deepsight.gg': { path: 'chirivulpes/conduit.deepsight.gg', branch: 'package' },
+				'kitsui': { repo: 'fluff4me/kitsui', branch: 'package' },
+				'conduit.deepsight.gg': { repo: 'chirivulpes/conduit.deepsight.gg', branch: 'package' },
 			},
 		},
 	)
