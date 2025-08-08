ea77b7cb7358a16d2f1e5116a1c94cbd27cfafda
Chiri Vulpes <chiri@chiri.works>
2025-08-09 09:57:45 +1200

    Add profile button to navbar, fix display bar transition out being delayed way too long

--- src/component/Navbar.ts
+++ src/component/Navbar.ts
@@ -1,6 +1,9 @@
 import Link from 'component/core/Link'
+import ProfileButton from 'component/profile/ProfileButton'
 import WordmarkLogo from 'component/WordmarkLogo'
 import { Component, State } from 'kitsui'
+import Slot from 'kitsui/component/Slot'
+import Profile from 'model/Profile'
 
 interface NavbarExtensions {
 	readonly visible: State.Mutable<boolean>
@@ -16,13 +19,21 @@ const Navbar = Component('nav', (component): Navbar => {
 	const homelink = Link('/')
 		.and(WordmarkLogo)
 		.style('navbar-homelink')
+		.appendTo(component)
+
+	Slot()
+		.appendTo(component)
+		.use(Profile.STATE, (slot, profiles) => profiles.selected
+			&& ProfileButton(profiles.selected)
+				.style('navbar-profile-button')
+				.tweak(button => button.mode.setValue('simple'))
+		)
 
 	viewTransitionsEnabled.use(component, enabled => {
 		homelink.viewTransition(enabled && 'wordmark-logo-home-link')
 	})
 
 	return component.style('navbar')
-		.append(homelink)
 		.extend<NavbarExtensions>(navbar => ({
 			visible,
 			viewTransitionsEnabled,
