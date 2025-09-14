5507803fa12cb388613360411889c3c60595905a
Chiri Vulpes <chiri@chiri.works>
2025-09-15 01:10:06 +1200

    Maybe fix icons in ci

--- task/icons.ts
+++ task/icons.ts
@@ -1,10 +1,14 @@
 import fs from 'fs/promises'
 import { Task } from 'task'
+import Env from './utility/Env'
 
 export default Task('icons', async task => {
 	await fs.mkdir('./icon/out', { recursive: true })
 	await task.exec('NPM:fantasticon', '--normalize=true')
 	const icons = await fs.readFile('./icon/out/icons.ts', 'utf8')
+	await fs.mkdir('./src/style', { recursive: true })
 	await fs.writeFile('./src/style/icons.ts', icons)
-	await task.exec('NPM:eslint', './src/style/icons.ts', '--fix')
+
+	if (Env.ENVIRONMENT === 'dev')
+		await task.exec('NPM:eslint', './src/style/icons.ts', '--fix')
 })
