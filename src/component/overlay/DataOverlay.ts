989bf65feb03699965f3cd24f093c56ac926f470
Chiri Vulpes <chiri@chiri.works>
2025-11-08 02:12:43 +1300

    Add support for object key links

--- src/component/overlay/DataOverlay.ts
+++ src/component/overlay/DataOverlay.ts
@@ -124,13 +124,14 @@ export default Component((component, params: State<DataOverlayParams | undefined
 			const pathString = path.join('/')
 			const highlighted = navigate.hash.map(component, hash => hash.startsWith(`#${pathString}`))
 
+			const keyString = typeof key !== 'object' ? `${key}` : undefined
 			let container: JSONContainer
 			const keyComponent = Component('a')
 				.setOwner(component)
 				.style('data-overlay-json-container-entry-key')
 				.attributes.set('href', `#${pathString}`)
 				.append(...typeof key === 'object' ? Arrays.resolve(key) : [])
-				.text.append(typeof key !== 'object' ? `${key}` : '')
+				.text.append(keyString ?? '')
 				.event.subscribe('click', e => {
 					if (e.targetComponent?.is(JSONCopyPaste))
 						return
@@ -174,6 +175,7 @@ export default Component((component, params: State<DataOverlayParams | undefined
 							? JSONPlaceholder(`{} ${expandable.size} ${expandable.size === 1 ? 'entry' : 'entries'}`)
 							: JSONPlaceholder(`[] ${expandable.length} ${expandable.length === 1 ? 'item' : 'items'}`))
 						)
+						.append(expandable && keyString && JSONLinks(keyString, path))
 						.event.subscribe('click', e => {
 							if (e.targetComponent?.is(JSONCopyPaste))
 								e.preventDefault()
@@ -419,106 +421,7 @@ export default Component((component, params: State<DataOverlayParams | undefined
 		const JSONNumber = Component((component, number: number, path?: (string | number)[]) => component.and(JSONComponent)
 			.style('data-overlay-json', 'data-overlay-json-number')
 			.append(JSONCopyPaste(number))
-			.append(Slot().use(links, (slot, links) => {
-				if (!links)
-					return
-
-				const pathString = path?.join('.') ?? ''
-
-				const { link } = links.links?.find(({ pathRegex }) => pathRegex.test(pathString)) ?? {}
-				if (!link)
-					return
-
-				const ref = Component()
-					.style('data-overlay-json-reference')
-					.appendTo(slot)
-
-				if ('enum' in link) {
-					const enumDef = links.enums?.[link.enum]
-
-					////////////////////////////////////
-					//#region Reference Enum
-
-					const enumMember = enumDef?.members.find(e => e.value === number)
-					if (enumDef && enumMember) ref
-						.style('data-overlay-json-reference-enum')
-						.append(JSONPunctuation('enum'))
-						.text.append(' ')
-						.append(Component()
-							.style('data-overlay-json-reference-enum-name')
-							.text.set(enumDef.name)
-						)
-						.append(JSONPunctuation('.'))
-						.append(Component()
-							.style('data-overlay-json-reference-enum-member')
-							.text.set(enumMember.name)
-						)
-
-					//#endregion
-					////////////////////////////////////
-
-					else if (enumDef?.bitmask) {
-						////////////////////////////////////
-						//#region Reference Bitmask
-
-						const enumMembers = number === 0
-							? enumDef.members.filter(e => e.value === 0)
-							: enumDef.members.filter(e => (e.value & number) === e.value && e.value !== 0)
-						if (enumMembers.length > 0) ref
-							.style('data-overlay-json-reference-enum')
-							.append(JSONPunctuation('bitmask'))
-							.text.append(' ')
-							.append(Component()
-								.style('data-overlay-json-reference-enum-name')
-								.text.set(enumDef.name)
-							)
-							.text.append(' ')
-							.append(JSONPunctuation('['))
-							.text.append(' ')
-							.append(...enumMembers.flatMap((e, i) => [
-								i && JSONPunctuation(' | '),
-								(Component()
-									.style('data-overlay-json-reference-enum-member')
-									.text.set(e.name)
-								),
-							]))
-							.text.append(' ')
-							.append(JSONPunctuation(']'))
-
-						//#endregion
-						////////////////////////////////////
-					}
-				}
-				else {
-					////////////////////////////////////
-					//#region Reference Def
-
-					const defs = links.definitions?.[link.component] as Partial<Record<number, object>> | undefined
-					const linkedDef = defs?.[number]
-					DataProvider.SINGLE.prep(link.component, number)
-					if (linkedDef) ref
-						.style('data-overlay-json-reference-definition')
-						.append(Link(`/data/${link.component}/${number}`)
-							.style('data-overlay-json-reference-definition-link')
-							.append(Component()
-								.style('data-overlay-json-reference-label')
-								.text.set(DataHelper.getComponentName(link.component))
-							)
-							.text.append(' "')
-							.append(Component()
-								.style('data-overlay-json-reference-definition-link-title')
-								.text.set(DataHelper.getTitle(link.component, linkedDef))
-							)
-							.text.append('"')
-						)
-
-					//#endregion
-					////////////////////////////////////
-				}
-
-				if (ref.element.childNodes.length)
-					slot.prepend(JSONPunctuation(' // '))
-			}))
+			.append(JSONLinks(number, path))
 		)
 
 		const JSONBool = Component((component, bool: boolean) => component.and(JSONComponent)
@@ -579,6 +482,112 @@ export default Component((component, params: State<DataOverlayParams | undefined
 			})
 		)
 
+		//#endregion
+		////////////////////////////////////
+
+		////////////////////////////////////
+		//#region Links
+
+		const JSONLinks = Component((component, key: string | number, path?: (string | number)[]) => component.and(Slot).use(links, (slot, links) => {
+			if (!links)
+				return
+
+			const pathString = path?.join('.') ?? ''
+
+			const { link } = links.links?.find(({ pathRegex }) => pathRegex.test(pathString)) ?? {}
+			if (!link)
+				return
+
+			const ref = Component()
+				.style('data-overlay-json-reference')
+				.appendTo(slot)
+
+			if ('enum' in link) {
+				const enumDef = links.enums?.[link.enum]
+
+				////////////////////////////////////
+				//#region Reference Enum
+
+				const enumMember = enumDef?.members.find(e => `${e.value}` === `${key}`)
+				if (enumDef && enumMember) ref
+					.style('data-overlay-json-reference-enum')
+					.append(JSONPunctuation('enum'))
+					.text.append(' ')
+					.append(Component()
+						.style('data-overlay-json-reference-enum-name')
+						.text.set(enumDef.name)
+					)
+					.append(JSONPunctuation('.'))
+					.append(Component()
+						.style('data-overlay-json-reference-enum-member')
+						.text.set(enumMember.name)
+					)
+
+				//#endregion
+				////////////////////////////////////
+
+				else if (enumDef?.bitmask) {
+					////////////////////////////////////
+					//#region Reference Bitmask
+
+					const enumMembers = `${key}` === '0'
+						? enumDef.members.filter(e => e.value === 0)
+						: enumDef.members.filter(e => (e.value & Number(key)) === e.value && e.value !== 0)
+					if (enumMembers.length > 0) ref
+						.style('data-overlay-json-reference-enum')
+						.append(JSONPunctuation('bitmask'))
+						.text.append(' ')
+						.append(Component()
+							.style('data-overlay-json-reference-enum-name')
+							.text.set(enumDef.name)
+						)
+						.text.append(' ')
+						.append(JSONPunctuation('['))
+						.text.append(' ')
+						.append(...enumMembers.flatMap((e, i) => [
+							i && JSONPunctuation(' | '),
+							(Component()
+								.style('data-overlay-json-reference-enum-member')
+								.text.set(e.name)
+							),
+						]))
+						.text.append(' ')
+						.append(JSONPunctuation(']'))
+
+					//#endregion
+					////////////////////////////////////
+				}
+			}
+			else {
+				////////////////////////////////////
+				//#region Reference Def
+
+				const defs = links.definitions?.[link.component] as Partial<Record<string | number, object>> | undefined
+				const linkedDef = defs?.[key]
+				DataProvider.SINGLE.prep(link.component, key)
+				if (linkedDef) ref
+					.style('data-overlay-json-reference-definition')
+					.append(Link(`/data/${link.component}/${key}`)
+						.style('data-overlay-json-reference-definition-link')
+						.append(Component()
+							.style('data-overlay-json-reference-label')
+							.text.set(DataHelper.getComponentName(link.component))
+						)
+						.text.append(' "')
+						.append(Component()
+							.style('data-overlay-json-reference-definition-link-title')
+							.text.set(DataHelper.getTitle(link.component, linkedDef))
+						)
+						.text.append('"')
+					)
+
+				//#endregion
+				////////////////////////////////////
+			}
+
+			if (ref.element.childNodes.length)
+				slot.prepend(JSONPunctuation(' // '))
+		}))
 		//#endregion
 		////////////////////////////////////
 
