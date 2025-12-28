4827a48becc50d32806b13ba022fb77dbd381517
Chiri Vulpes <chiri@chiri.works>
2025-12-29 11:58:00 +1300

    Don't display background on engrams

--- src/component/item/Item.ts
+++ src/component/item/Item.ts
@@ -1,12 +1,13 @@
 import Button from 'component/core/Button'
 import Image from 'component/core/Image'
 import ItemTooltip from 'component/tooltip/ItemTooltip'
+import { ItemCategoryHashes } from 'deepsight.gg/Enums'
+import type { DeepsightTierTypeDefinition } from 'deepsight.gg/Interfaces'
 import { Component, State } from 'kitsui'
 import type Tooltip from 'kitsui/component/Tooltip'
 import DisplayProperties from 'model/DisplayProperties'
 import type { ItemState } from 'model/Item'
 import Moment from 'model/Moment'
-import type { DeepsightTierTypeDefinition } from 'node_modules/deepsight.gg/Interfaces'
 
 interface ItemExtensions {
 	readonly state: State<ItemState>
@@ -20,12 +21,14 @@ const Item = Object.assign(
 
 		const masterworked = state.map(component, item => false)
 		const featured = state.map(component, item => item.definition.featured)
+		const isEngram = state.map(component, item => !!item.definition.categoryHashes?.includes(ItemCategoryHashes.Engrams))
 		const rarity = state.map(component, ({ provider: collections, definition }): DeepsightTierTypeDefinition | undefined => collections.rarities[definition.rarity])
 
 		component.and(Button)
 		component.style('item')
 		component.style.bindFrom(rarity.map(component, rarity => rarity && `item--${rarity.displayProperties.name!.toLowerCase()}` as 'item--common'))
 		component.style.bind(masterworked, 'item--masterworked')
+		component.style.bind(isEngram, 'item--engram')
 
 		Component()
 			.style('item-border')
