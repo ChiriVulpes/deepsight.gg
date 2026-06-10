1eec57449c5c4aa7754066be49514f56ea22ad13
Chiri Vulpes <chiri@chiri.works>
2026-06-11 10:41:11 +1200

    Fix rarity sort order

--- src/component/display/sort/definition/SortRarity.ts
+++ src/component/display/sort/definition/SortRarity.ts
@@ -1,8 +1,20 @@
-import { Sort, SortDefinitionCompare } from 'component/display/sort/SortDefinition'
+import { Sort } from 'component/display/sort/SortDefinition'
+import { ItemTierTypeHashes } from 'deepsight.gg/Enums'
+
+const rarityOrder = [
+	ItemTierTypeHashes.Invalid,
+	ItemTierTypeHashes.BasicCurrency,
+	ItemTierTypeHashes.BasicQuest,
+	ItemTierTypeHashes.Common,
+	ItemTierTypeHashes.Uncommon,
+	ItemTierTypeHashes.Rare,
+	ItemTierTypeHashes.Legendary,
+	ItemTierTypeHashes.Exotic,
+]
 
 export default Sort.Definition({
 	id: 'rarity',
 	label: quilt => quilt['display-bar/sort/rarity/title'](),
 	shortLabel: quilt => quilt['display-bar/sort/rarity/short'](),
-	compare: (a, b) => SortDefinitionCompare.compareNumber(a.definition?.rarity, b.definition?.rarity),
+	compare: (a, b) => rarityOrder.indexOf(b.definition?.rarity!) - rarityOrder.indexOf(a.definition?.rarity!),
 })
